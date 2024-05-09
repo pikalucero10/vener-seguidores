b58c527e142d149bcc54a4982c53e5efde288787
pikalucero10 <166959276+pikalucero10@users.noreply.github.com>
2024-05-09 15:03:45 -0300

    Update conversor.js

--- conversor.js
+++ conversor.js
@@ -1,14 +1,27 @@
 // Función para convertir dólares a pesos
 function convertirAPesos() {
-    const cantidadUSD = parseFloat(
-      document.getElementById("cantidadDolares").value
-    );
-    const tipoCambioActual = parseFloat(
-      document.getElementById("tipoCambio").value
-    ); // Obtener el tipo de cambio actual
-    const cantidadPesos = cantidadUSD * tipoCambioActual;
-    document.getElementById("resultadoPesos").innerText = `${cantidadUSD.toFixed(
-      2
-    )} dólares son ${cantidadPesos.toFixed(2)} pesos.`;
-  }
-  
+  const cantidadUSD = parseFloat(
+    document.getElementById("cantidadDolares").value
+  );
+  const tipoCambioActual = parseFloat(
+    document.getElementById("tipoCambio").value
+  ); // Obtener el tipo de cambio actual
+  const cantidadPesos = cantidadUSD * tipoCambioActual;
+  document.getElementById("resultadoPesos").innerText = `${cantidadUSD.toFixed(
+    2
+  )} dólares son ${cantidadPesos.toFixed(2)} pesos.`;
+}
+
+// Función para convertir pesos a dólares
+function convertirADolares() {
+  const cantidadPesos = parseFloat(
+    document.getElementById("cantidadPesos").value
+  );
+  const tipoCambioActual = parseFloat(
+    document.getElementById("tipoCambio").value
+  ); // Obtener el tipo de cambio actual
+  const cantidadUSD = cantidadPesos / tipoCambioActual;
+  document.getElementById("resultadoDolares").innerText = `${cantidadPesos.toFixed(
+    2
+  )} pesos son ${cantidadUSD.toFixed(2)} dólares.`;
+}
